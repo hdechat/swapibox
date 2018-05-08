8ac3e176e1c29c720dfad1a5cdab67b09904aacc
Helen Dechat <hdechat@gmail.com>
2018-05-08 00:25:20 -0600

    Add full fetch for API with variable representing number from 1 to 7

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import cleanFilmData from '../cleaners.js'
-import filmData from '../mockFilmData.js'
+// import filmData from '../mockFilmData.js'
 import TextScroll from '../TextScroll/TextScroll.js'
 
 export default class App extends Component {
@@ -14,8 +14,17 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    const textCrawl = cleanFilmData(filmData);
-    this.setState({ textCrawl });
+    //mock data
+    // const textCrawl = cleanFilmData(filmData);
+    // this.setState({ textCrawl });
+
+    let randomFilm = Math.floor(Math.random() * 7 + 1)
+     fetch(`https://swapi.co/api/films/${randomFilm}`)
+    .then(data => data.json())
+    .then(data => {
+      const textCrawl = cleanFilmData(data);
+      this.setState({ textCrawl });
+    })
   }
 
   render() {
