fbbddcb39a4715f966990112897d487f32635f1f
Helen Dechat <hdechat@gmail.com>
2018-05-08 16:32:03 -0600

    Add Button components to App render and edit typos

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import PropTypes form 'prop-types';
+import PropTypes from 'prop-types';
 import './Button.css';
 
 const Button = ({ category, callback }) => {
   return(
-    <button onclick={}>{category}</button>
+    <button onclick={()=>{}}>{category}</button>
   )
 }
 
 Button.propTypes = {
-  category = PropTypes.string,
-  callback = PropType.func
+  category: PropTypes.string,
+  callback: PropTypes.func
 }
 
 export default Button;
