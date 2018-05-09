56d990bed083434af820c6c411ccd5b5806cf16d
Helen Dechat <hdechat@gmail.com>
2018-05-09 03:10:33 -0600

    Create fetch methods for people cards and Card render

--- src/App/App.js
+++ src/App/App.js
@@ -18,17 +18,50 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    
+
     const textCrawl = cleanFilmData(filmData);
     this.setState({ textCrawl });
 
     // let randomFilm = Math.floor(Math.random() * 7 + 1)
     //  fetch(`https://swapi.co/api/films/${randomFilm}`)
-    // .then(data => data.json())
+    // .then(response => response.json())
     // .then(data => {
     //   const textCrawl = cleanFilmData(data);
     //   this.setState({ textCrawl });
     // })
+  }
+
+  createPeopleData = (people) => {
+    const peopleData = people.reduce((arr, person) => {
+      const nameAndHome = fetch(person.homeworld)
+      .then(response => response.json())
+      .then(data => ({name: person.name, homeworld: data.name, population: data.population}));
+
+      if(person.species.length > 0) {
+      const species = fetch(person.species[0])
+      .then(response => response.json())
+      .then(data => ({name: person.name, species: data.name}));
+      return [...arr, nameAndHome, species]
+      } else {
+        return [...arr, nameAndHome, {name: person.name, species: person.species}]
+      }
+    }, []);
+
+    return Promise.all(peopleData);
+  }
+
+  fetchPeopleList = () => {
+    for(let i = 1; i < 10; i++) { 
+      fetch(`https://swapi.co/api/people/?page=${i}`)
+      .then(response => response.json())
+      .then(data => this.createPeopleData(data.results))
+      .then(cards => {
+          const mergedCards = cards.reduce((arr, card, index) => {
+          return [...arr, {...card, ...cards[index+1]}]
+        }, []);
+        this.setState({ cardList: [ ...this.state.cardList, ...mergedCards] });
+      });
+    }
   }
   
   render() {
@@ -38,10 +71,12 @@ export default class App extends Component {
           <TextScroll textCrawl={this.state.textCrawl} />
         </aside>
         <main>
+          <div className="buttons">
           <Button category={"favorites"} callback={()=>{}} />
-          <Button category={"People"} callback={()=>{}} />
+          <Button category={"People"} callback={this.fetchPeopleList} />
           <Button category={"Vehicles"} callback={()=>{}} />
           <Button category={"Planets"} callback={()=>{}} />
+          </div>
           <CardContainer cardList={this.state.cardList}/>
         </main>
       </div>
--- src/CardContainer/Card/Card.js
+++ src/CardContainer/Card/Card.js
@@ -5,7 +5,11 @@ import './Card.css';
 const Card = ({ card }) => {
   return (
     <article>
-      THIS IS A CARD FOOL!
+     <h2>{card.name}</h2>
+     <h5>{card.species}</h5>
+     <h5>{card.homeworld}</h5>
+     <p>pop: {card.population}</p>
+
     </article>
   )
 }
