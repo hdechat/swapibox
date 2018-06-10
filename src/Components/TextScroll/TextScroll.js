986acadd4a6f00cad7570961a14eb15b44e6af65
Helen Dechat <hdechat@gmail.com>
2018-06-09 19:45:19 -0600

    Add scroll and music

--- src/Components/TextScroll/TextScroll.js
+++ src/Components/TextScroll/TextScroll.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import starWarsAudio from '../../assets/star-wars.mp3'
 import './TextScroll.css'
 
 const TextScroll = ({ textCrawl }) => {
   return (
-    <div>
-      <div className="textScroll">
-        <p>{textCrawl.text}</p>
-        <p>{textCrawl.title}</p>
-        <p>{textCrawl.released}</p>
-      </div>
+    <div className="textcrawl-container">
+      <audio autoPlay>
+        <source src={starWarsAudio} type="audio/mpeg" />
+      </audio>
       <button onClick={() => document.querySelector('aside').style.display = 'none'}>Click</button>
+      <section className="star-wars">
+        <div className="crawl">
+          <p>{textCrawl.text}</p>
+          <p>{textCrawl.title}</p>
+          <p>{textCrawl.released}</p>
+        </div>
+      </section>
     </div>
   )
 }
